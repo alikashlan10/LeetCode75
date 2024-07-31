229612a854be001d40e09c85b975aa9c79041508
Ali Mohamed <76254631+alikashlan10@users.noreply.github.com>
2024-07-31 17:57:22 +0300

    605.CanPlaceFlowers

--- src/Solution.java
+++ src/Solution.java
@@ -10,11 +10,32 @@ public class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
 
         int FlowersCntr =0 ;
+        if(flowerbed.length ==1 )
+        {
+            if(flowerbed[0]==0)
+                return true;
+        }
         for(int i=0;i<flowerbed.length;i++) {
-            if(flowerbed[i]==0 && flowerbed[i+1]==0 && flowerbed[i-1]==0 ) {
-                FlowersCntr++;
+            if(i==flowerbed.length-1)
+            {
+                if(flowerbed[i]==0 && flowerbed[i-1] ==0)
+                    FlowersCntr++;
+                flowerbed[i]=1;
+            }
+            else if (i==0){
+                if(flowerbed[i]==0 && flowerbed[i+1]==0)
+                {
+                    FlowersCntr++;
+                    flowerbed[i]=1;
+                }
+            }
+            else{
+                if(flowerbed[i]==0 && flowerbed[i+1]==0 && flowerbed[i-1]==0 ) {
+                    FlowersCntr++;
+                    flowerbed[i]=1;
+                }
             }
         }
-        return n==FlowersCntr;
+        return n<=FlowersCntr;
     }
 }
