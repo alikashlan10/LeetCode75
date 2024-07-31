4689c2b5d3b69396635f559f8acfa1f9e2311967
Ali Mohamed <76254631+alikashlan10@users.noreply.github.com>
2024-07-31 18:55:19 +0300

    345.Reverse vowels of a string

--- src/Solution.java
+++ src/Solution.java
@@ -1,41 +1,37 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
-//605.CanPlaceFlowers
+//345.Reverse vowels of a string
 //Easy
 //Time Complexity : O(N)
+//Space Complexity : O(N)
 
 
 public class Solution {
-    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+    public String reverseVowels(String s) {
 
-        int FlowersCntr =0 ;
-        if(flowerbed.length ==1 )
-        {
-            if(flowerbed[0]==0)
-                return true;
-        }
-        for(int i=0;i<flowerbed.length;i++) {
-            if(i==flowerbed.length-1)
-            {
-                if(flowerbed[i]==0 && flowerbed[i-1] ==0)
-                    FlowersCntr++;
-                flowerbed[i]=1;
+        Stack<Character> placeHolder = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u'
+                    || s.charAt(i)=='A' || s.charAt(i)=='E' || s.charAt(i)=='I' || s.charAt(i)=='O' || s.charAt(i)=='U'){
+                placeHolder.push(s.charAt(i));
             }
-            else if (i==0){
-                if(flowerbed[i]==0 && flowerbed[i+1]==0)
-                {
-                    FlowersCntr++;
-                    flowerbed[i]=1;
-                }
+        }
+
+        //int pointer=0;
+        StringBuilder reversed = new StringBuilder(s);
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u'
+               || s.charAt(i)=='A' || s.charAt(i)=='E' || s.charAt(i)=='I' || s.charAt(i)=='O' || s.charAt(i)=='U'){
+                if(!placeHolder.isEmpty()){}
+                reversed.setCharAt(i,placeHolder.pop());
             }
             else{
-                if(flowerbed[i]==0 && flowerbed[i+1]==0 && flowerbed[i-1]==0 ) {
-                    FlowersCntr++;
-                    flowerbed[i]=1;
-                }
+                reversed.setCharAt(i,s.charAt(i));
             }
         }
-        return n<=FlowersCntr;
+
+        return reversed.toString();
     }
 }
