8eb5cb803f7c13a10a7137ef82e5e3c641ab7f53
Ali Mohamed <76254631+alikashlan10@users.noreply.github.com>
2024-08-01 00:23:39 +0300

    238.ProductOfArrayExceptSelf

--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,32 @@
 import java.util.*;
 
-//151.Reverse words of string
+//238.ProductOfArrayExceptSelf
 //Medium
 //Time Complexity : O(N)
-//Space Complexity : O(N)
+//Space Complexity : O(1)
+// beats 99.96 %
 
 
-public class Solution {
-    public String reverseWords(String s) {
-        // Trim the input string and split by one or more spaces
-        List<String> words = Arrays.asList(s.trim().split("\\s+"));
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int length = nums.length;
+        int[] res = new int[length];
 
-        // Reverse the list of words
-        Collections.reverse(words);
+        res[0] = 1;
 
-        // Join the words back into a single string with a space separator
-        return String.join(" ", words);
+        //left (prefix)
+        for (int i = 1; i < length; i++) {
+            res[i] = nums[i - 1] * res[i - 1];
+        }
+
+        // Calculate the right (suffix) product and combine it with the left product
+        int rightProduct = 1;
+        for (int i = length - 1; i >= 0; i--) {
+            res[i] = res[i] * rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return res;
     }
 }
+
