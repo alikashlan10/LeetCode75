3efc59415e87676e9d995d9d6cdecff2ddadedf7
Ali Mohamed <76254631+alikashlan10@users.noreply.github.com>
2024-09-03 06:03:03 +0300

    334.IncreasingTriplet

--- src/Solution.java
+++ src/Solution.java
@@ -1,32 +1,29 @@
 import java.util.*;
+import java.util.stream.IntStream;
 
-//238.ProductOfArrayExceptSelf
-//Medium
+//1679.max number of k sum pairs
+//medium
 //Time Complexity : O(N)
 //Space Complexity : O(1)
-// beats 99.96 %
 
 
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        int length = nums.length;
-        int[] res = new int[length];
-
-        res[0] = 1;
-
-        //left (prefix)
-        for (int i = 1; i < length; i++) {
-            res[i] = nums[i - 1] * res[i - 1];
+    public int maxOperations(int[] nums, int k) {
+        int ops = 0 ;
+        int left = 0;
+        int right = nums.length - 1;
+        Arrays.sort(nums);
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == k) {
+                right--;
+                left++;
+                ops++;
+            }
+            else if (sum < k)
+                right--;
+            else left++;
         }
-
-        // Calculate the right (suffix) product and combine it with the left product
-        int rightProduct = 1;
-        for (int i = length - 1; i >= 0; i--) {
-            res[i] = res[i] * rightProduct;
-            rightProduct *= nums[i];
-        }
-
-        return res;
+        return ops;
     }
 }
-
